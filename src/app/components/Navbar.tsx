073df30eef65a513b18c11c6a2e8213725905c90
Panagitis Alexandro Tsilfidis <tsilfidis@gmail.com>
2025-03-21 10:03:06 -0400

    chore: ensure aprimorated css in pages

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between bg-primary text-secondary-light h-[100px] pb-3 px-4">
       <div className="w-full flex justify-between items-center">
-        <div className="flex items-center space-x-2">
+        <Link href="/" className="flex items-center space-x-2 hover:scale-105">
           <Image
             src="/logo-EB-400px.png"
             width={80}
@@ -15,47 +15,28 @@ const Navbar = () => {
             priority={true}
           />
           <h1 className="font-semibold">Educando com a Bíblia</h1>
-        </div>
+        </Link>
         <div id="menu_links" className="hidden md:block">
           <ul className="flex space-x-2 text- items-center">
-            <li>
-              <Link
-                href="/"
-                passHref
-                className="hover:border border-secondary-light rounded-2xl p-2"
-              >
+            <li className="p-2 hover:opacity-50 hover:scale-105">
+              <Link href="/" passHref>
                 Home
               </Link>
             </li>
-            <li>
-              <Link
-                href="/blog"
-                className="hover:border hover:border-secondary-light rounded-2xl p-2"
-              >
-                Blog
-              </Link>
+            <li className="p-2 hover:opacity-50 hover:scale-105">
+              <Link href="/blog">Blog</Link>
             </li>
-            <li>
-              <Link
-                href="/produtos"
-                className="hover:border border-secondary-light rounded-2xl p-2"
-              >
-                Produtos
-              </Link>
+            <li className="p-2 hover:opacity-50 hover:scale-105">
+              <Link href="/produtos">Produtos</Link>
             </li>
-            <li>
-              <Link
-                href="/quem-somos"
-                className="hover:border border-secondary-light rounded-2xl p-2"
-              >
-                Quem Somos
-              </Link>
+            <li className="p-2 hover:opacity-50 hover:scale-105">
+              <Link href="/quem-somos">Quem Somos</Link>
             </li>
-            <li className="hidden">
+            <li className="hidden hover:opacity-50 hover:scale-105 hover:text-secondary-light">
               <Link href="/inscricoes">
                 <Button
                   variant="destructive"
-                  className="font-semibold rounded-2xl hover:border hover:border-secondary-light"
+                  className="font-semibold rounded-2xl hover:bg-primary hover:border hover:border-secondary-light"
                 >
                   Inscrições
                 </Button>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -58,6 +58,5 @@ export default {
       },
     },
   },
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
   plugins: [require("tailwindcss-animate")],
 } satisfies Config;
